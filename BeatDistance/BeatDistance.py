00034e5a6805443f9e578950a1920c2ea5fb532d
JLStack <stackjl@appstate.edu>
2015-02-17 16:31:51 -0500

    added documentation for BeatDistance.py

--- BeatDistance/BeatDistance.py
+++ BeatDistance/BeatDistance.py
@@ -1,9 +1,9 @@
 __author__ = 'lukestack'
-
 import math
+import echonest.remix
 
 """
-This class allows you to compare two 'echonest.remix.audio.AudioQuantum'. 
+This a way of comparing two 'echonest.remix.audio.AudioQuantum' beats.
 This method of comparing beats is almost identical to the way Paul Lamere compares
 beats in the InfiniteJukebox. Only minor changes have been made.
 """
@@ -15,23 +15,45 @@ loudMaxWeight = 1
 durationWeight = 100
 confidenceWeight = 1
 
+"""
+These are the weights decided on by Paul Lamere.
+Weights can be thought of as level of importance when calculating distance.
+For more on the specifications of these fields, visit http://developer.echonest.com/docs/v4/_static/AnalyzeDocumentation.pdf
+"""
+
 def get_beat_distance(beat1, beat2):
-    if type(beat1) != echonest.remix.audio.AudioQuantum or type(beat2) != echonest.remix.audio.AudioQuantum or \
+    """
+    The only method the user should ever call.
+    The distance between beats is really just the average distance between the segments of the two beats.
+
+    :param beat1: first beat to be compared
+    :param beat2: second beat to be compared
+    :return: a numeric value for the distance between beats
+    """
+    if type(beat1) != echonest.remix.audio.AudioQuantum or type(beat1) != echonest.remix.audio.AudioQuantum or \
                     beat1.kind != "beat" or beat2.kind != "beat":
         raise Exception("make sure both parameters are type <'echonest.remix.audio.AudioQuantum'>' and of kind 'beat'")
     if len(beat1.segments) > len(beat2.segments):
         segs = len(beat2.segments)
     else:
         segs = len(beat1.segments)
-    average = 0
-    for seg in range(0,segs):
-        average += get_seg_distance(beat1.segments[seg], beat2.segments[seg])
-    average = average / segs
+    total = 0
+    for seg in range(0, segs):
+        total += __get_seg_distance__(beat1.segments[seg], beat2.segments[seg]) #adds each distance between individual segments to the total
+    average = total / segs #takes the average distance between segments
     return average
 
-def get_seg_distance(seg1, seg2):
-    timbre = seg_distance(seg1, seg2, 'timbre')
-    pitch = seg_distance(seg1, seg2, 'pitches')
+
+def __get_seg_distance__(seg1, seg2):
+    """
+    Takes the distance between individual segments.
+
+    :param seg1: first segment to be compared
+    :param seg2: second segment to be compared
+    :return: a numeric value for the distance between segments
+    """
+    timbre = __seg_distance__(seg1, seg2, 'timbre')
+    pitch = __seg_distance__(seg1, seg2, 'pitches')
     sloudStart = math.fabs(seg1.loudness_begin - seg2.loudness_begin)
     sloudMax = math.fabs(seg1.loudness_max - seg2.loudness_max)
     duration = math.fabs(seg1.duration - seg2.duration)
@@ -39,18 +61,38 @@ def get_seg_distance(seg1, seg2):
         confidence = math.fabs(seg1.confidence - seg2.confidence)
     else:
         confidence = 0
+    #takes the individual field differences and multiplies them by their corresponding weights to get the overall distance
     distance = (timbre * timbreWeight) + (pitch * pitchWeight) + (sloudStart * loudStartWeight) + \
                (sloudMax * loudMaxWeight) + (duration * durationWeight) + (confidence * confidenceWeight)
     return distance
 
-def seg_distance(seg1, seg2, field):
-    if field == 'timbre':
-        return euclidean_distance(seg1.timbre, seg2.timbre)
-    return euclidean_distance(seg1.pitches, seg2.pitches)
+def __seg_distance__(seg1, seg2, field):
+    """
+    Helper method to call the __euclidean_distance__() with the
+    right list of values based on the field given
 
-def euclidean_distance(v1, v2):
+    :param seg1: first segment to be compared
+    :param seg2: second segment to be compared
+    :param field: either timbre or pitch
+    :return: distance between list of values for pitch or timbre
+    """
+    if field == 'timbre':
+        return __euclidean_distance__(seg1.timbre, seg2.timbre)
+    return __euclidean_distance__(seg1.pitches, seg2.pitches)
+
+
+def __euclidean_distance__(v1, v2):
+    """
+    Think of the distance formula in geometry. It's the same thing.
+    sqrt((q1-p1)**2 + (q2-p2)**2 + ...)
+
+    :param v1: list of values from seg1
+    :param v2: list of values from seg2
+    :return: the euclidean distance between the two lists
+    """
     sum = 0
-    for i in range(0,len(v1)):
+    for i in range(0, len(v1)):
         delta = v2[i] - v1[i]
         sum += delta * delta
     return math.sqrt(sum)
+
--- BeatDistance/README.md
+++ BeatDistance/README.md
@@ -12,8 +12,66 @@ import BeatDistance
 audio_file = audio.LocalAudioFile("15 Sir Duke.m4a")
 beats = audio_file.analysis.beats
 
-beat1 = beats.__getitem__(0)
-beat2 = beats.__getitem__(1)
+beat1 = beats[0]
+beat2 = beats[1]
 
-print BeatDistance.get_beat_distance(beat1, beat2)
+print BeatDistance.get_beat_distance(beat1, beat2) #returns a numeric value for the "distance" between the two
+```
+
+**Code Explanation**
+
+First, there are predefined weights for different fields. These are the same weights Paul Lamere decided on when implementing the InfiniteJukebox. Weights can be thought of as level of importance. Keep in mind that different fields have different ranges. A field may contribute more to the distance measurement even though it's weight is less than another. Timbre is the best example of this. It's weight is only 1, yet it has the greatest range of all the fields. Therefore, it plays the greatest importance when calculating distance.
+
+```python
+timbreWeight = 1
+pitchWeight = 10
+loudStartWeight = 1
+loudMaxWeight = 1
+durationWeight = 100
+confidenceWeight = 1
+```
+
+In my method, I look to see which beat has the fewest number of segments. I then only compare this many number of segments. 
+
+```python
+if len(beat1.segments) > len(beat2.segments):
+        segs = len(beat2.segments)
+    else:
+        segs = len(beat1.segments)
+    total = 0
+    for seg in range(0, segs):
+        total += __get_seg_distance__(beat1.segments[seg], beat2.segments[seg]) #adds each distance between individual segments to the total
+    average = total / segs #takes the average distance between segments
+    return average
+```
+
+Paul Lamere's InfiniteJukebox implementation. Paul Lamere compares the number of segments located in beat1. If beat2 does not have at least as many segments as beat1, 100's are added in place of the actual distance between segments. 
+
+```javascript
+for (var j = 0; j < q1.overlappingSegments.length; j++) {
+            var seg1 = q1.overlappingSegments[j];
+            var distance = 100;
+            if (j < q2.overlappingSegments.length) {
+                var seg2 = q2.overlappingSegments[j];
+                // some segments can overlap many quantums,
+                // we don't want this self segue, so give them a
+                // high distance
+                if (seg1.which === seg2.which) {
+                    distance = 100
+                } else {
+                    distance = get_seg_distances(seg1, seg2);
+                }
+            } 
+            sum += distance;
+        }
+```
+
+You may have noticed __seg_distance__(seg1, seg2, field) being called. __seg_distance__() just calls the __euclidean_distance__() with the right list of values based on the field given. The field will either be 'timbre' or 'pitch'. __euclidean_distance__() uses the same distance formula everyone uses in high school geometry. sqrt((q1-p1)**2 + (q2-p2)**2 + ...)
+
+```python
+sum = 0
+    for i in range(0, len(v1)):
+        delta = v2[i] - v1[i]
+        sum += delta * delta
+    return math.sqrt(sum)
 ```
