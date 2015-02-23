247042957b245c033b59aaa95384ba4e2e518617
J. Luke Stack <stackjl@appstate.edu>
2015-02-23 16:24:20 -0500

    Delete README.py

--- InfiniteJukeboxReplica/README.py
+++
@@ -1,3 +0,0 @@
-##InfiniteJukeboxReplica
-
-InfiniteJukeboxReplica is a Python implementation of Paul Lamere's InfiniteJukebox. Some liberties were taken to simplify the code. Just run the InfiniteJukeBoxApp.py. A gui will pop up. Just put the path of your song into the textbox and choose your threshold. Threshold can be thought of as the quality of the branches. The lower the threshold, the better the branches will sound. 
