a0073a1fd2f34ff9ece024d37dbb4912b0319279
JLStack <stackjl@appstate.edu>
2015-03-31 10:14:17 -0400

    added TempoShift

---
+++ TempoShift/TempoShift.py
@@ -0,0 +1,24 @@
+__author__ = 'lukestack'
+
+import dirac
+from echonest.remix import audio
+from aqplayer import Player
+
+file1 = "15 Sir Duke.m4a"
+
+def main():
+    audiofile = audio.LocalAudioFile(file1)
+    player = Player(audiofile)
+    beats = audiofile.analysis.beats
+    for beat in beats:
+        ratio = 1.25 - ((float(beat.absolute_context()[0]) / float(len(beats))) * .5)
+        print ratio
+        beat_audio = beat.render()
+        scaled_beat = dirac.timeScale(beat_audio.data, ratio)
+        ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape,
+                                     sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
+        player.play_from_AudioData(ts)
+
+if __name__ == '__main__':
+    main()
+
