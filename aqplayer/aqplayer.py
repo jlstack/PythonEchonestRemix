fb563466cda7d48f3d43aba0e723a56eefcb8858
JLStack <stackjl@appstate.edu>
2015-04-08 15:21:05 -0400

    changed how aqplayer works

--- aqplayer/aqplayer.py
+++ aqplayer/aqplayer.py
@@ -12,69 +12,40 @@ class Player:
     Plays an echonest.remix.audio.AudioQuantum given an echonest.remix.audio.LocalAudioFile.
     It opens a pyaudio stream and feeds it the wave frames to be played.
     """
-    def __init__(self, audio_file):
-        self.af = audio_file
-        self.wf = wave.open(self.get_wav(), 'rb')
+    def __init__(self):
         if sys.platform == 'linux2':
-            # make sure you've started this with $ padsp python infinite_playlist [...]
             import ossaudiodev
             self.stream = ossaudiodev.open('w')
-            self.stream.setparameters(ossaudiodev.AFMT_S16_LE, self.af.numChannels, self.af.sampleRate)
+            self.stream.setparameters(ossaudiodev.AFMT_S16_LE, 2, 44100)
         else:
             import pyaudio
             self.p = pyaudio.PyAudio()
-            self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
-                                 channels=self.af.numChannels, rate=self.af.sampleRate, output=True)
+            self.stream = self.p.open(format=self.p.get_format_from_width(2), channels=2, rate=44100, output=True)
 
-    def get_wav(self):
-        # borrowed some code from the AudioData class to get wavfile
+    def shift_tempo_and_play(self, AudioQuantum, ratio):
         """
-        Helper method for __init__
-        :return .wav file from the LocalAudioFile
+        Takes an echonest.remix.audio.AudioQuantum and a ratio.
+        It first shifts the tempo of the AudioQuantum using dirac
+        and then writes the modified data to the stream.
         """
-        if self.af.filename.lower().endswith(".wav") and (self.af.sampleRate, self.af.numChannels) == (44100, 2):
-            file_to_read = self.af.filename
-        elif self.af.convertedfile:
-            file_to_read = self.af.convertedfile
-        else:
-            temp_file_handle, self.af.convertedfile = tempfile.mkstemp(".wav")
-            self.af.sampleRate, self.af.numChannels = ffmpeg(self.af.filename, self.af.convertedfile, overwrite=True,
-                                                             numChannels=self.af.numChannels,
-                                                             sampleRate=self.af.sampleRate, verbose=self.af.verbose)
-            file_to_read = self.af.convertedfile
-        return file_to_read
+        import dirac
+        import numpy as np
+        ad = AudioQuantum.render()
+        scaled_beat = dirac.timeScale(ad.data, ratio)
+        self.stream.write(scaled_beat.astype(np.int16).tostring())
 
-    def play_from_AudioData(self, AudioData):
+    def play(self, audio_quantum):
         """
-        Feeds data from AudioData object into stream.
-        This allows real time tempo shifting to occur.
+        Accepts any echonest.remix.audio.AudioQuantum and audibly plays it for you using pyaudio.
         """
-        frames = AudioData.data.tostring()
-        self.stream.write(frames)
-
-    def play(self, audio_quantum, intro=False):
-        """
-        Accepts any echonest.remix.audio.AudioQuantum and audibly plays it for you.
-        If the AudioQuantum is the first one present, it will play any frames before
-        it's start time. To turn this off, set intro=False.
-        """
-        index = audio_quantum.absolute_context()[0]
-        if index == 0 and intro:
-            numframes = int((audio_quantum.duration + audio_quantum.start) * self.wf.getframerate())
-            startframe = 0
-        else:
-            numframes = int(audio_quantum.duration * self.wf.getframerate())
-            startframe = int(audio_quantum.start * self.wf.getframerate())
-        self.wf.setpos(startframe)
-        self.stream.write(self.wf.readframes(numframes))
+        ad = audio_quantum.render()
+        self.stream.write(ad.data.tostring())
 
     def close_stream(self):
         """
         closes pyaudio stream
         """
         self.stream.close()
-        self.wf.close()
         self.af.unload()
         if self.p is not None:
             self.p.terminate()
-
