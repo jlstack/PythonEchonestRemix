08759e679d002524f145e60b7d72885565dbb6ce
JLStack <stackjl@appstate.edu>
2015-04-29 01:00:52 -0400

    updated AnalysisVisualization.py

--- AnalysisVisualization/AnalysisVisualization.py
+++ AnalysisVisualization/AnalysisVisualization.py
@@ -2,93 +2,86 @@ __author__ = 'lukestack'
 
 import echonest.remix.audio as audio
 import matplotlib.pyplot as plt
-import numpy as np
 import SegColor
 import aqplayer
 import threading
-import time
-from matplotlib.pyplot import pause, ion, ioff
-import matplotlib.patches as mpatches
-
 
 
 audio_file = audio.LocalAudioFile("MP3Songs/15 Sir Duke.m4a")
-print audio_file.analysis.tempo['value']
 audio_data = audio_file.render().data
 audio_data = (audio_data[:, 0] + audio_data[:, 1]) / 2
 bounds = SegColor.normalizeColor(audio_file)
 
-tatums = audio_file.analysis.tatums
 segments = audio_file.analysis.segments
+tatums = audio_file.analysis.tatums
 beats = audio_file.analysis.beats
 bars = audio_file.analysis.bars
 sections = audio_file.analysis.sections
 
-condensed_data = []
-
-for tat in tatums:
-    beginning = int(tat.start * 44100)
-    end = int((tat.start + tat.duration) * 44100)
-    data = audio_data[beginning:end]
-    condensed_data.append((np.amin(data), np.amax(data)))
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
-
-for sect in sections:
-    ax.plot([sect.start, sect.start], [0, 60], linewidth=4, color='black')
-
-for bar in bars:
-    ax.plot([bar.start, bar.start], [7.5, 52.5], linewidth=3.5, color='purple')
-
-for beat in beats:
-    ax.plot([beat.start, beat.start], [15, 45], linewidth=3, color='green')
-
-for tat in tatums:
-    ax.plot([tat.start, tat.start], [22.5, 37.5], linewidth=2.5, color='red')
 
 for i in range(0, len(segments)):
     start = segments[i].start
     loudest_point = start + segments[i].time_loudness_max
     end = start + segments[i].duration
     color = SegColor.getSegmentColor(bounds, segments[i])
-    if i != len(segments) - 1:
-        ax.plot([start, loudest_point], [segments[i].loudness_begin, segments[i].loudness_max], linewidth=2, color=color)
-        ax.plot([loudest_point, end], [segments[i].loudness_max, segments[i + 1].loudness_begin], linewidth=2, color=color)
+    if i + 1 < len(segments):
+        next_seg_loudness = segments[i + 1].loudness_begin
     else:
-        ax.plot([start, loudest_point], [segments[i].loudness_begin, segments[i].loudness_max], linewidth=2, color=color)
-        ax.plot([loudest_point, end], [segments[i].loudness_max, 0], linewidth=2, color=color)
+        next_seg_loudness = 0
+    ax.plot([start, loudest_point], [segments[i].loudness_begin, segments[i].loudness_max], linewidth=2, color=color)
+    ax.fill_between([start, loudest_point], [segments[i].loudness_begin, segments[i].loudness_max], -60, color=color)
+    ax.plot([loudest_point, end], [segments[i].loudness_max, next_seg_loudness], linewidth=2, color=color)
+    ax.fill_between([loudest_point, end], [segments[i].loudness_max, next_seg_loudness], -60, color=color)
 
-ax.plot([sections[0].start, sections[0].start], [0, 60], linewidth=4, color='black', label="Section")
-ax.plot([bars[0].start, bars[0].start], [7.5, 52.5], linewidth=3.5, color='purple', label="Bar")
-ax.plot([beats[0].start, beats[0].start], [15, 45], linewidth=3, color='green', label="Beat")
-ax.plot([tatums[0].start, tatums[0].start], [22.5, 37.5], linewidth=2.5, color='red', label="Tatum")
+for sect in sections:
+    ax.plot([sect.start, sect.start], [-30, -60], linewidth=4, color='black')
 
-def play_beat(beat, player):
-    player.play(beat)
+for bar in bars:
+    ax.plot([bar.start, bar.start], [-37.5, -60], linewidth=3.5, color='blue')
 
-ax.set_xlim([0, 30])
-ax.legend()
-plt.show()
-
-#still need to make it scroll
-"""
-dt = 0.001
-timer = fig.canvas.new_timer(interval=dt*1000.0)
-cursor, = ax.plot(0, 0, color='green', markersize=10, marker='s')
-player = aqplayer.Player()
-plt.show()
-beat = beats[0]
-
-thread_list = []
 for beat in beats:
-    thread = threading.Thread(target=play_beat, args=(beat, player))
-    thread_list.append(thread)
+    ax.plot([beat.start, beat.start], [-45, -60], linewidth=3, color='green')
 
-for i in range(len(beats)):
-    thread_list[i].start()
-    cursor.set_xdata(beats[i].start)
+for tat in tatums:
+    ax.plot([tat.start, tat.start], [-52.5, -60], linewidth=2.5, color='red')
+
+ax.plot([sections[0].start, sections[0].start], [-30, -60], linewidth=4, color='black', label="Section")
+ax.plot([bars[0].start, bars[0].start], [-37.5, -60], linewidth=3.5, color='blue', label="Bar")
+ax.plot([beats[0].start, beats[0].start], [-45, -60], linewidth=3, color='green', label="Beat")
+ax.plot([tatums[0].start, tatums[0].start], [-52.5, -60], linewidth=2.5, color='red', label="Tatum")
+
+ax.set_xlim([0, 10])
+ax.set_ylim(-60, 0)
+ax.legend()
+plt.show(block=True)
+
+
+# need help making it run
+
+
+def update_cursor(cursor, x):
+    cursor.set_xdata(x)
     fig.canvas.draw()
 
-player.close_stream()
-"""
+
+def play_music():
+    player = aqplayer.Player()
+    beat = beats[0]
+    cursor, = ax.plot(0, -55, color='green', markersize=10, marker='s')
+    dt = 0.001
+    timer = fig.canvas.new_timer(interval=dt*1000.0)
+    timer.add_callback(update_cursor, cursor, beat.start)
+    timer.start()
+    for b in beats:
+        beat = b
+        player.play(beat)
+    player.close_stream()
+
+
+# play_music()
+# thread1 = threading.Thread(target=play_music)
+# thread2 = threading.Thread(target=run_cursor)
+# thread2.start()
+# thread1.start()
