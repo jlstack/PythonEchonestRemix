9300ca4c5d11d0561e007a67bd40a100d3ca54bf
JLStack <stackjl@appstate.edu>
2015-04-30 23:00:33 -0400

    updated TempoShift.py to work with new aqplayer

--- TempoShift/TempoShift.py
+++ TempoShift/TempoShift.py
@@ -1,24 +1,19 @@
 __author__ = 'lukestack'
 
-import dirac
 from echonest.remix import audio
 from aqplayer import Player
 
-file1 = "15 Sir Duke.m4a"
 
 def main():
+    file1 = "Mp3Songs/15 Sir Duke.m4a"
     audiofile = audio.LocalAudioFile(file1)
-    player = Player(audiofile)
+    player = Player()
     beats = audiofile.analysis.beats
     for beat in beats:
         ratio = 1.25 - ((float(beat.absolute_context()[0]) / float(len(beats))) * .5)
-        print ratio
-        beat_audio = beat.render()
-        scaled_beat = dirac.timeScale(beat_audio.data, ratio)
-        ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape,
-                                     sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
-        player.play_from_AudioData(ts)
+        player.shift_tempo_and_play(beat, ratio)
     player.close_stream()
+
+
 if __name__ == '__main__':
     main()
-
